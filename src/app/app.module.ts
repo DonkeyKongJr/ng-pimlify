cef5eae42881e62ffa87248300e99ffce95219e0
Patrick Schadler <pschadler@pmone.com>
2018-11-28 15:37:34 +0100

    feat(restaurant-overview): add retrieval of all restaurants and display as a simple list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantOverviewComponent } from './views/restaurant-overview/restaurant-overview.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RestaurantOverviewComponent
-  ],
+  declarations: [AppComponent, RestaurantOverviewComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'pimlify'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/views/restaurant-overview/restaurant-overview.component.ts
+++ src/app/views/restaurant-overview/restaurant-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-restaurant-overview',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-overview.component.scss']
 })
 export class RestaurantOverviewComponent implements OnInit {
+  public restaurants: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(firestore: AngularFirestore) {
+    this.restaurants = firestore.collection('restaurant').valueChanges();
   }
 
+  ngOnInit() {}
 }
