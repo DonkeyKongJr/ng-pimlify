1c9b8b0fc1fcb9b0231755ce88b249d1c63062a4
Patrick Schadler <pschadler@pmone.com>
2018-11-28 15:02:37 +0100

    feat(restaurant-overview): add restaurant overview component and routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantOverviewComponent } from './views/restaurant-overview/restaurant-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantOverviewComponent
   ],
   imports: [
     BrowserModule,
